f711c51972ad37085a6c5dec1dd8812d44099007
Topy <gmsdl1994@naver.com>
2022-08-25 23:12:03 +0900

    Session 07 ~ 08

--- src/main/java/JpaStudy_02/JpaStudy01Application.java
+++ src/main/java/JpaStudy_02/JpaStudy01Application.java
@@ -1,12 +1,22 @@
 package JpaStudy_02;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaStudy01Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpaStudy01Application.class, args);
+		}
+
+		@Bean
+		Hibernate5Module hibernate5Module() {
+			Hibernate5Module hibernate5Module = new Hibernate5Module();
+//			hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+			return hibernate5Module;
 	}
 }
---
+++ src/main/java/JpaStudy_02/api/OrderSimpleApiController.java
@@ -0,0 +1,67 @@
+package JpaStudy_02.api;
+
+import JpaStudy_02.domain.Address;
+import JpaStudy_02.domain.Order;
+import JpaStudy_02.domain.OrderStatus;
+import JpaStudy_02.repository.OrderRepository;
+import JpaStudy_02.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
